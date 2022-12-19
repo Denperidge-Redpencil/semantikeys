696c151588b1c2cdb349b30a82e17e1dd104e8d7
Denperidge <denperidge@gmail.com>
2022-12-19 17:00:21 +0100

    Renamed Audio to Menu

--- frontend/tests/integration/components/menu-test.js
+++ frontend/tests/integration/components/menu-test.js
@@ -3,22 +3,22 @@ import { setupRenderingTest } from 'frontend/tests/helpers';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
-module('Integration | Component | audio', function (hooks) {
+module('Integration | Component | menu', function (hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function (assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
 
-    await render(hbs`<Audio />`);
+    await render(hbs`<Menu />`);
 
     assert.dom(this.element).hasText('');
 
     // Template block usage:
     await render(hbs`
-      <Audio>
+      <Menu>
         template block text
-      </Audio>
+      </Menu>
     `);
 
     assert.dom(this.element).hasText('template block text');
