69c446932e698568adffc0dc47a49b3920c32a43
Denperidge <denperidge@gmail.com>
2022-12-20 16:56:22 +0100

    Added application wide conditional return to map

--- frontend/app/routes/application.js
+++ frontend/app/routes/application.js
@@ -1,17 +1,38 @@
 import Route from '@ember/routing/route';
 import { service } from '@ember/service';
+import { tracked } from '@glimmer/tracking';
 import { set } from '@ember/object';
+import { action } from '@ember/object';
+
+function checkNav(currentRouteName) {
+  let renderNav = true;
+  if (currentRouteName.startsWith('thirteen') || currentRouteName.startsWith('start') || currentRouteName.startsWith('navigation')) {
+    renderNav = false;
+  }
+
+  return renderNav;
+}
 
 export default class ApplicationRoute extends Route {
   @service router;
   @service menuService;
+  @tracked opts;
 
-  model() {
+  model(params, transition) {
+
+    this.opts = {
+      nav: checkNav(transition.to.name)
+    }
+
     this.router.on('routeDidChange', (transition) => {
-      console.log(transition.to.name);
+      this.opts.nav = checkNav(transition.to.name);
+
+
       //this.menuService.music = transition.to.name;
       set(this.menuService, 'music', transition.to.name);
       //document.querySelector('#music-component audio').src = `/audio/music/${transition.to.name}.mp3`;
     });
+    return this.opts;
+    
   }
 }
