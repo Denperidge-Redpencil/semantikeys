f2d880a00e231a678e2ce39fac2b64d793871413
Denperidge <denperidge@gmail.com>
2022-12-30 01:20:42 +0100

    WIP thirteen.pt1 test

--- frontend/tests/unit/routes/thirteen/pt1-test.js
+++ frontend/tests/unit/routes/thirteen/pt1-test.js
@@ -1,13 +1,31 @@
+import {
+  click,
+  currentURL,
+  fillIn,
+  waitFor,
+  visit,
+  currentRouteName,
+  waitUntil,
+  pauseTest,
+  resumeTest,
+  settled,
+  find,
+  triggerEvent
+} from '@ember/test-helpers';
 import { module, test } from 'qunit';
 import { setupTest } from 'frontend/tests/helpers';
+import { testSelector } from 'frontend/tests/helpers/custom-helpers';
 
 module('Unit | Route | thirteen/pt1', function (hooks) {
   setupTest(hooks);
 
-  /*
-  test('it exists', function (assert) {
-    let route = this.owner.lookup('route:thirteen/pt1');
-    assert.ok(route);
+  test('Continue becomes visible', function (assert) {
+    assert.dom(testSelector('continue')).hasStyle({opacity: 1});
+/*
+     waitUntil(() => {
+      return find(testSelector('continue')).style.opacity == 1;
+    }, { timeout: 10000 });
+    */
+    
   });
-  */
 });
