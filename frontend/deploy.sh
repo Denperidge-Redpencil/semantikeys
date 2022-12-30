c6ec7c867efc13a00c1c1c38e64340ad0b9c218f
Denperidge <denperidge@gmail.com>
2022-12-30 01:50:56 +0100

    Reduced sleep time for deploy.sh

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # This fixes ember surge cli not auto exiting, thus forever keeping the GitHub Action going
 npx ember surge &
-sleep 300
+sleep 180
 #until npx ember surge | grep -m 1 "Success!"; do : ; sleep 10; done;
