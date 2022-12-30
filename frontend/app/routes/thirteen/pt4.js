8a794b39160be6e43a5e42adc3bcafd700eea02e
Denperidge <denperidge@gmail.com>
2022-12-30 16:06:28 +0100

    pt4 timeout now handled in route instead of templ
    This fixes a global failure bug caused by the code in the template
    still trying to run,
    after the route has been navigated through during testing

--- frontend/app/routes/thirteen/pt4.js
+++ frontend/app/routes/thirteen/pt4.js
@@ -1,9 +1,12 @@
 import Route from '@ember/routing/route';
 import { later, cancel } from '@ember/runloop';
 import { service } from '@ember/service';
+import { action } from '@ember/object';
 
 export default class ThirteenPt4Route extends Route {
   @service router;
+  contTimeout;
+
   model() {
     return {
       array: [
@@ -27,12 +30,22 @@ export default class ThirteenPt4Route extends Route {
     };
   }
 
+  @action
+  displayCont() {
+    this.contTimeout = setTimeout(function() {
+      document.getElementById('cont').style.display = 'initial';
+    }, 4000);
+  }
+
   afterModel() {
+    this.displayCont();
+
     let delay = setTimeout(() => {
       this.router.transitionTo('thirteen.pt3');
     }, 5500);
     this.router.on('routeWillChange', (transition) => {
       clearTimeout(delay);
+      clearTimeout(this.contTimeout);
     });
   }
 }
