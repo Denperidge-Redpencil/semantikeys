60294cc205691e38adbc15fed55c681a09e8b7cc
Denperidge <denperidge@gmail.com>
2022-12-30 22:03:46 +0100

    Added route tests for thirteen.pt3 & pt4

--- frontend/tests/application/routes/thirteen-test.js
+++ frontend/tests/application/routes/thirteen-test.js
@@ -57,4 +57,64 @@ module('Acceptance | thirteen', function (hooks) {
     assert.ok(correctOptions == 1, 'There is only 1 option with the correct href.');
     assert.ok(options.length > correctOptions, 'There are more options than correct ones.');
   });
+
+  test('pt3', async function (assert) {
+    await visitRoute(this, 'thirteen.pt3');
+
+    let dimensions = ['left', 'top', 'right', 'bottom'];
+    let correctSteps = dimensions.map((dimension) => dimension = `${dimension} in bounds: `);
+    for (let i = 4; i >= 1; i--) {
+      correctSteps.splice(i, 0, 'true');
+    }
+    /* correctSteps =
+        [
+          "left in bounds: ",
+          true,
+          "top in bounds: ",
+          true,
+          "right in bounds: ",
+          true,
+          "bottom in bounds: ",
+          true
+        ]
+    */
+
+    let svgRect = find('svg').getBoundingClientRect();
+    let continueRect = find(testSelector('continue')).getBoundingClientRect();
+
+
+    dimensions.forEach((dimension) => {
+      assert.step(`${dimension} in bounds: `)
+      assert.step(`${svgRect[dimension] < continueRect[dimension]}`)
+    });
+
+    
+
+    assert.verifySteps(correctSteps, 'The continue button is within the SVG\'s box.');
+  });
+
+  test('pt4', async function (assert) {
+    await visitRoute(this, 'thirteen.pt4');
+
+    await waitUntil(() => {
+      return currentRouteName() == 'thirteen.pt3';
+    },
+    { 
+      timeout: 10000,
+      timeoutMessage: 'thirteen.pt4 auto-redirect without clicking isn\' working as expected.'
+    });
+    assert.ok(currentRouteName() == 'thirteen.pt3', 'Without interaction, pt4 automatically redirects to pt3');
+    
+    await visitRoute(this, 'thirteen.pt4');
+    let clickTarget = find('#cont');
+    await waitUntil(() => {
+      return getComputedStyle(clickTarget).display != 'none';
+    },
+    {
+      timeout: 10000,
+      timeoutMessage: 'The clickable target stays on display: none.'
+    });
+    await click(clickTarget);
+    assert.ok(currentRouteName() == 'thirteen.pt5', 'After clicking on the target, pt4 navigates to pt5');
+  });
 });
