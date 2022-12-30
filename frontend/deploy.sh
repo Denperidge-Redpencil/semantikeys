b2b02835da57c406e1cadf95e11d4812a0f8da66
Denperidge <denperidge@gmail.com>
2022-12-30 01:21:17 +0100

    Added missing npx operand to deploy.sh

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This fixes ember surge cli not auto exiting, thus forever keeping the GitHub Action going
-until ember surge | grep -m 1 "Success!"; do : ; sleep 10; done;
+until npx ember surge | grep -m 1 "Success!"; do : ; sleep 10; done;
